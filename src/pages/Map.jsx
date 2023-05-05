366df28e460d0f1189756d65fd4a9ec7e4ce2527
zshancs <mahmad.zeeshan72@gmail.com>
2023-05-06 00:07:24 +0500

    markers on regions added

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,12 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, ImageOverlay, useMap, useMapEvents, } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Tooltip, ImageOverlay, useMap, useMapEvents, Circle, } from "react-leaflet";
 import BaseLayout from '../../components/BaseLayout/BaseLayout';
 
 import Sidebar from '../../components/zMapboxSidebar';
 import { useRouter } from 'next/router'
 import "leaflet/dist/leaflet.css";
 import { latLng } from 'leaflet';
-
+import L from 'leaflet'
 import { mosaic_data } from './api/alldata';
 
 
@@ -35,20 +35,16 @@ const Explore = () => {
     }
 
 
-
+    //if some date doesnt exists -> use first date
     useEffect(
         () => {
             if (!mosaic_data[variant][date])
                 setDate(Object.getOwnPropertyNames(mosaic_data[variant])[1]);
         },
         [variant]
-    )
-    // useEffect(()=>{
-    //     onVariablesChange({ variant, date, filter });
-    // })
+    );
 
     useEffect(() => {
-        // This function will be called every time the location changes
         onVariablesChange({ variant, date, filter })
     }, [variant, date, filter]);
 
@@ -67,10 +63,13 @@ const Explore = () => {
         });
 
     }
+    
+    const [mylatLng, setLatLng] = useState([32.33994977092287, 72.53458678722383]);
+
     function Location() {
 
         const map = useMapEvents({
-            click: (e) => { console.log(e); }
+            click: (e) => { navigator.clipboard.writeText([...Object.values(e.latlng)]); console.log(Object.values(e.latlng)); setLatLng([e.latlng.lat, e.latlng.lng]) }
         })
     }
 
@@ -97,8 +96,76 @@ const Explore = () => {
                             opacity={opacity}
                         />
                         <MapFly overlayBounds={mosaic_data[variant][date]['mosaic']['overlayBounds']} />
+
+
+                        {mosaic_data[variant][date]['healthy']?.map(latLong => {
+                            return (
+                                <Marker
+                                    key={latLong[0]}
+                                    position={latLong}
+                                    icon={L.icon({
+                                        iconUrl: 'leaf-green.png',
+                                        shadowUrl: 'leaf-shadow.png',
+
+
+                                        iconSize: [38 / 2, 95 / 2], // size of the icon
+                                        shadowSize: [50 / 2, 64 / 2], // size of the shadow
+                                        iconAnchor: [22 / 2, 94 / 2], // point of the icon which will correspond to marker's location
+                                        shadowAnchor: [4 / 2, 62 / 2],  // the same for the shadow
+                                        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+                                    })}>
+                                    <Tooltip>This Region seems healtheir than the average</Tooltip>
+                                </Marker>
+                            )
+                        })}
+
+
+
+
+                        {mosaic_data[variant][date]['unhealthy']?.map(latLong => {
+                            return (
+                                <Marker
+                                    key={latLong[0]}
+                                    position={latLong}
+                                    icon={L.icon({
+                                        iconUrl: 'leaf-red.png',
+                                        shadowUrl: 'leaf-shadow.png',
+
+
+                                        iconSize: [38 / 2, 95 / 2], // size of the icon
+                                        shadowSize: [50 / 2, 64 / 2], // size of the shadow
+                                        iconAnchor: [22 / 2, 94 / 2], // point of the icon which will correspond to marker's location
+                                        shadowAnchor: [4 / 2, 62 / 2],  // the same for the shadow
+                                        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+                                    })}>
+                                    <Tooltip>This Region seems to be suffering from stunted growth</Tooltip>
+                                </Marker>
+                            )
+                        })}
                     </>
-                )}
+                )
+                }
+
+
+                <Marker
+                    position={mylatLng}
+                    icon={L.icon({
+                        iconUrl: 'leaf-orange.png',
+                        shadowUrl: 'leaf-shadow.png',
+
+
+                        iconSize: [38 / 2, 95 / 2], // size of the icon
+                        shadowSize: [50 / 2, 64 / 2], // size of the shadow
+                        iconAnchor: [22 / 2, 94 / 2], // point of the icon which will correspond to marker's location
+                        shadowAnchor: [4 / 2, 62 / 2],  // the same for the shadow
+                        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+                    })}>
+                    <Tooltip>This Region is unknown</Tooltip>
+                </Marker>
+
                 <Location />
             </MapContainer>
 
@@ -110,7 +177,7 @@ const Explore = () => {
                     <>
                         <div className="fixed top-2 left-1/2 -translate-x-1/2 rounded-xl text-xl text-white bg-black bg-opacity-30 backdrop-filter backdrop-blur-md px-3 py-3 z-20 ">
                             <h1>Growth Stage: {mosaic_data[variant][date]['stage']}</h1>
-                            <div onClick={(e) => { let dates = Object.getOwnPropertyNames(mosaic_data[variant]); let curr = dates.indexOf(date); let next = dates[curr - 1]; if (next) { setDate(next) } }}  className={` ${Object.getOwnPropertyNames(mosaic_data[variant]).indexOf(date) == 0 ? 'hidden' : 'cursor-pointer'}  absolute top-1/2 -left-6 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black bg-opacity-40 px-3 py-2  backdrop-filter backdrop-blur-md `}>
+                            <div onClick={(e) => { let dates = Object.getOwnPropertyNames(mosaic_data[variant]); let curr = dates.indexOf(date); let next = dates[curr - 1]; if (next) { setDate(next) } }} className={` ${Object.getOwnPropertyNames(mosaic_data[variant]).indexOf(date) == 0 ? 'hidden' : 'cursor-pointer'}  absolute top-1/2 -left-6 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black bg-opacity-40 px-3 py-2  backdrop-filter backdrop-blur-md `}>
                                 ◀
                             </div>
                             <div onClick={(e) => { let dates = Object.getOwnPropertyNames(mosaic_data[variant]); let curr = dates.indexOf(date); let next = dates[curr + 1]; if (next) { setDate(next) } }} className={`  ${Object.getOwnPropertyNames(mosaic_data[variant]).indexOf(date) == Object.getOwnPropertyNames(mosaic_data[variant]).length - 1 ? 'hidden' : 'cursor-pointer'} cursor-pointer absolute top-1/2 -right-16 -translate-x-1/2 rotate-180 -translate-y-1/2 rounded-full bg-black bg-opacity-40 px-3 py-2  backdrop-filter backdrop-blur-md `}>
@@ -138,7 +205,7 @@ const Explore = () => {
 
                 }
             </div>
-        </BaseLayout>
+        </BaseLayout >
 
     );
 }
